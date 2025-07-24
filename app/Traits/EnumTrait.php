f94614182b908140e6a54757740829863423caca
Shmuel Bisberg <shmuelbisberg.acc@gmail.com>
2025-07-24 20:55:06 +0300

    traits: Add EnumTrait with common methods for enums

---
+++ app/Traits/EnumTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Trait for enums with common methods.
+ */
+trait EnumTrait
+{
+    public static function foo(): string
+    {
+        return __('foo');
+    }
+
+    public static function toCollection(): Collection
+    {
+        /**
+         * Convert enum cases to a collection with additional properties.
+         */
+        return collect(self::cases())
+            ->map(fn ($case) => [
+                'value' => $case->value,
+                'name' => $case->name,
+                'label' => method_exists($case, 'label') ? $case->label() : null,
+                'description' => method_exists($case, 'description') ? $case->description() : null,
+                'color' => method_exists($case, 'color') ? $case->color() : null,
+            ]);
+    }
+}
